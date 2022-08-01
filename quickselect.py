0b77eadd12b700f0907f5ee36ce80129ff0b291d
Johnkhk <khh019@ucsd.edu>
2022-07-31 20:50:11 -0700

    first commit

---
+++ quickselect.py
@@ -0,0 +1,16 @@
+def partition(arr, l, r):
+      
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+          
+        if arr[j] <= x:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+              
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+
+nums =[3,2,5,4,1,23,11]
+ans = partition(nums,0,len(nums)-1)
+print(ans,nums)
